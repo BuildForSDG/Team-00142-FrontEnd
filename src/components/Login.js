a1dd3eec49deb97ec06c56968fbdcfe215073527
wuodmwalimu <wilsonochieng718@gmail.com>
2020-05-14 17:21:27 +0300

    added a Login javascrip file in the components directory

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,27 @@
 import React,{Component} from 'react'
 class Login extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+        username: ''
+    }
+}
+handleUsernameChange = () =>{
+    this.setState({
+username:event.target.value
+    } )
+}
+
     render(){
         return(
             <form>
             <div>
             <label>Username </label>
-            <input type='text'/>
+            <input
+             type='text'
+             value={this.state.username}
+             onChange={this.handleUsernameChange}
+             />
             </div>
             </form>
         )
