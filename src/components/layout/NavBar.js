526f0d3305e8f8e1506186198b893831511ef330
Chiuri254 <joseshiory@gmail.com>
2020-05-30 08:30:00 +0300

    Update component layout

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -8,19 +8,19 @@ const NavBar = () => {
       <a href="#!" class="brand-logo">Logo</a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">Javascript</a></li>
-        <li><a href="mobile.html">Mobile</a></li>
+        <li><a href="sass.html">Home</a></li>
+        <li><a href="badges.html">About</a></li>
+        <li><a href="collapsible.html">Project</a></li>
+        <li><a href="mobile.html">Register</a></li>
         <li><a href="sass.html"><i class="material-icons left">search</i>Link with Left Icon</a></li>
         <a class="waves-effect waves-light btn orange lighten-4"><i class="material-icons right">redo</i>Log in</a>
       </ul>
     </div>
     <ul class="sidenav" id="mobile-demo">
-    <li><a href="sass.html">Sass</a></li>
-    <li><a href="badges.html">Components</a></li>
-    <li><a href="collapsible.html">Javascript</a></li>
-    <li><a href="mobile.html">Mobile</a></li>
+    <li><a href="sass.html">Home</a></li>
+    <li><a href="badges.html">About</a></li>
+    <li><a href="collapsible.html">Projects</a></li>
+    <li><a href="mobile.html">Register</a></li>
     <li><a href="sass.html"><i class="material-icons left">search</i>Link with Left Icon</a></li>
     <a class="waves-effect waves-light btn orange lighten-4"><i class="material-icons right">redo</i>Log in</a>
   </ul>
