2e98d47ae31131ce22eb3f630608b07754f41217
martin-wachira <martinnwachira@gmail.com>
2020-06-08 20:52:31 +0300

    Title Change

--- src/components/community/Community.js
+++ src/components/community/Community.js
@@ -42,19 +42,17 @@ export default class Community extends Component {
         <div key={item.id}>
           <h1 className="center">Community Based Projects</h1>
           <div className="row">
-
             <div className="col s3 m4">
               <span className="card-title"><h4>{item.projectName}</h4>by Riser Africa</span>
               <div className="card">
                 <div className="card-image">
                   <img src="https://images.unsplash.com/photo-1517976487492-5750f3195933?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"></img>
-
                 </div>
                 <div className="card-content">
                   <p className="truncate">{item.typeOfProject}</p>
                 </div>
                 <div className="card-content">
-                  {/* <p className="truncate">{item.typeOfAssistanceRequired}</p> */}
+                  <p className="truncate">{item.typeOfAssistanceRequired}</p>
                 </div>
                 <div className="card-action">
                   <a href="#" className="blue-text">READ</a>
