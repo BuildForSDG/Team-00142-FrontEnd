7465556ede602abe39dda74ea921f275935f52e0
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-06-15 19:37:53 +0300

    Added more changes

--- src/components/group/Group.js
+++ src/components/group/Group.js
@@ -26,7 +26,7 @@ class Group extends Component {
   render() {
     let projectList = this.state.projects.map((item) => {
       return (
-        <div key={item.id}>        
+        <div class="container" key={item.id}>        
           <div className="card center">
             <div className="col s3 m4">
               <span className="card-title"><h4>{item.projectName}</h4>by Riser Africa</span>
@@ -63,7 +63,7 @@ class Group extends Component {
 
     return (
       <div className="row">
-        <h2>Group Based Projects</h2>
+        <h3 class = "heading">Group Based Projects</h3>
          <div className="col-md-3">{projectList}</div>
       </div>
     )
